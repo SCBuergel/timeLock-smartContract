38d7b7b1374ed0ef3be4ff5bdef1364ca253d839
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2016-10-07 08:25:16 +0200

    Updated to match version with comments

--- timeLockNoComments.sol
+++ timeLockNoComments.sol
@@ -8,35 +8,41 @@ contract LockTimeContract
 
     mapping (address => accountData) accounts;
 
-    function payIn(uint lockTimeS) returns (bool x)
+    function payIn(uint lockTimeS)
     {
         uint amount = msg.value;
         payOut();
-        if (accounts[msg.sender].balance != 0)
-        {
-            msg.sender.send(amount);
-            return false;
-        }
-        accounts[msg.sender].balance = amount;
-        accounts[msg.sender].releaseTime = now + lockTimeS;
-        return true;
+	if (accounts[msg.sender].balance > 0)
+	    msg.sender.send(msg.value);
+	else
+	{
+	    accounts[msg.sender].balance = amount;
+            accounts[msg.sender].releaseTime = now + lockTimeS;
+	}
     }
     
     function payOut()
     {
         if (accounts[msg.sender].balance != 0 && accounts[msg.sender].releaseTime < now)
+        {
             msg.sender.send(accounts[msg.sender].balance);
+	    accounts[msg.sender].balance = 0;
+	    accounts[msg.sender].releaseTime = 0;
+        }
     }
-    
+
     function getMyLockedFunds() constant returns (uint x)
     {
         return accounts[msg.sender].balance;
     }
     
     function getMyLockedFundsReleaseTime() constant returns (uint x)
-      {
-          return accounts[msg.sender].releaseTime;
-      }
-    
- }
- 
+    {
+	return accounts[msg.sender].releaseTime;
+    }
+
+    function getNow() constant returns (uint x)
+    {
+	return now;
+    }
+}
