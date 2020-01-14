6e0a094969e789ef4ad68738060d13d78352d53c
Salt Bae <jazzholicbae@gmail.com>
2020-01-15 05:56:43 +0900

    Update timeLockNoComments.sol

--- timeLockNoComments.sol
+++ timeLockNoComments.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.5.16;
+
 contract timeLock
 {
     struct accountData
@@ -8,40 +10,40 @@ contract timeLock
 
     mapping (address => accountData) accounts;
 
-    function payIn(uint lockTimeS)
+    function payIn(uint lockTimeS) public payable
     {
         uint amount = msg.value;
         payOut();
         if (accounts[msg.sender].balance > 0)
-            msg.sender.send(msg.value);
+            msg.sender.transfer(msg.value);
         else
         {
             accounts[msg.sender].balance = amount;
             accounts[msg.sender].releaseTime = now + lockTimeS;
         }
     }
-    
-    function payOut()
+
+    function payOut() public
     {
         if (accounts[msg.sender].balance != 0 && accounts[msg.sender].releaseTime < now)
         {
-            msg.sender.send(accounts[msg.sender].balance);
+            msg.sender.transfer(accounts[msg.sender].balance);
             accounts[msg.sender].balance = 0;
             accounts[msg.sender].releaseTime = 0;
         }
     }
 
-    function getMyLockedFunds() constant returns (uint x)
+    function getMyLockedFunds() public view returns (uint x)
     {
         return accounts[msg.sender].balance;
     }
-    
-    function getMyLockedFundsReleaseTime() constant returns (uint x)
+
+    function getMyLockedFundsReleaseTime() public view returns (uint x)
     {
         return accounts[msg.sender].releaseTime;
     }
-    
-    function getNow() constant returns (uint x)
+
+    function getNow() public view returns (uint x)
     {
         return now;
     }
